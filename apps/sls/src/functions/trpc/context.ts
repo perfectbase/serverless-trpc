e9394844014587c21849eab41bdb0a1f4f46096c
Ravi Souza <perfectbase@users.noreply.github.com>
2022-07-18 10:37:14 +0900

    change to express

--- apps/sls/src/functions/trpc/context.ts
+++ apps/sls/src/functions/trpc/context.ts
@@ -1,7 +1,7 @@
-import { inferAsyncReturnType } from '@trpc/server';
-import { CreateAWSLambdaContextOptions } from '@trpc/server/adapters/aws-lambda';
-import { APIGatewayProxyEventV2 } from 'aws-lambda';
+// import { inferAsyncReturnType } from '@trpc/server';
+// import { CreateAWSLambdaContextOptions } from '@trpc/server/adapters/aws-lambda';
+// import { APIGatewayProxyEventV2 } from 'aws-lambda';
 
-export const createContext =
-  ({}: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => ({});
-export type Context = inferAsyncReturnType<typeof createContext>;
+// export const createContext =
+//   ({}: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => ({});
+// export type Context = inferAsyncReturnType<typeof createContext>;
--- apps/sls/src/functions/trpc/handler.ts
+++ apps/sls/src/functions/trpc/handler.ts
@@ -1,8 +1,47 @@
-import { awsLambdaRequestHandler } from '@trpc/server/adapters/aws-lambda';
-import { createContext } from './context';
-import { appRouter } from './routers';
+import { Context, Handler } from "aws-lambda";
+import serverlessExpress from "@vendia/serverless-express";
+import cors from "cors";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
+import { expressHandler } from "trpc-playground/handlers/express";
+import { AppRouter, appRouter } from "./routers";
 
-export const main = awsLambdaRequestHandler({
-  router: appRouter,
-  createContext,
-});
+let serverlessExpressInstance: any;
+const app = express();
+app.use(cors());
+
+const trpcApiEndpoint = "/trpc";
+const playgroundEndpoint = "/trpc-playground";
+
+app.use(
+  trpcApiEndpoint,
+  trpcExpress.createExpressMiddleware<AppRouter>({
+    router: appRouter,
+  })
+);
+
+async function setup(event: unknown, context: Context) {
+  app.use(
+    playgroundEndpoint,
+    await expressHandler({
+      trpcApiEndpoint,
+      playgroundEndpoint,
+      router: appRouter,
+      renderOptions: {
+        cdnUrl: "http://localhost:45245",
+        version: null,
+      },
+    })
+  );
+  serverlessExpressInstance = serverlessExpress({ app });
+  return serverlessExpressInstance(event, context);
+}
+
+export const main: Handler = (event, context) => {
+  if (serverlessExpressInstance)
+    return serverlessExpressInstance(event, context);
+
+  return setup(event, context);
+};
+
+// export const main = serverlessExpress({ app });
